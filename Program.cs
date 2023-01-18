2756fbb405f30e4a31a486fd09b219dca8f902e0
Darya <dasha.890@yandex.ru>
2023-01-18 22:56:03 +0500

    выполнили третье задание

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ Console.Write ($"Число А в степени В: {degree(A,B)}.");
 */
 
 //Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+/*
 int summary (int x, int y) {
     int sum = 0;
     for (int i = 0; i<=y; i++) {
@@ -40,3 +41,30 @@ for (int i = 0; i<=A; i++) {
       }
       }
 Console.Write ($"Сумма цифр в числе: {summary(A, y)}.");
+*/
+
+//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int[] GenerateArray(int length, int minNum, int maxNum)
+{
+    int[] arr = new int[length];
+    for (int i=0;i<arr.Length;i++)
+    {
+        arr[i] = new Random().Next(minNum, maxNum + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i=0;i<array.Length;i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int arr_len = 8;
+Console.Write ("Введите число минимального значения числа массива: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите число максимального значения числа массива: ");
+int B = Convert.ToInt32(Console.ReadLine());
+PrintArray(GenerateArray(arr_len, A, B));
