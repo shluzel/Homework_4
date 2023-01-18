4bd5724da2350f464b1d2c7c2b3b39143c17e130
Darya <dasha.890@yandex.ru>
2023-01-18 22:42:14 +0500

    Выполнили второе задание

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Clear();
 //Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-
+/*
 int degree (int x, int y) {
     int f = x;
     for (int i = 1; i<Math.Abs(y); i++) {
@@ -13,3 +13,30 @@ int A = Math.Abs(Convert.ToInt32(Console.ReadLine()));
 Console.Write ("Введите число B: ");
 int B = Math.Abs(Convert.ToInt32(Console.ReadLine()));
 Console.Write ($"Число А в степени В: {degree(A,B)}.");
+*/
+
+//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int summary (int x, int y) {
+    int sum = 0;
+    for (int i = 0; i<=y; i++) {
+        if (x%10>0) {
+        sum=sum+x%10;
+        x=x/10;
+      }
+      else {
+        sum=sum+x;
+        }
+}
+return sum;
+}
+Console.Write ("Введите число: ");
+int A = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int y=0;
+int x=A;
+for (int i = 0; i<=A; i++) {
+        if (x/10>0) {
+        y++;
+        x=x/10;
+      }
+      }
+Console.Write ($"Сумма цифр в числе: {summary(A, y)}.");
